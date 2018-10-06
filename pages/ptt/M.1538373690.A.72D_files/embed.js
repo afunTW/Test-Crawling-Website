f99fd6c3a48a3d3da9c0c7ceae2a871f3bee3f82
afunTW <endlessproof@gmail.com>
2018-10-06 11:11:03 +0800

    add ptt pratice image crawling

---
+++ pages/ptt/M.1538373690.A.72D_files/embed.js
@@ -0,0 +1,2 @@
+var host=window.location.host,source="s.imgur.com/min/embed-controller.js";if(!window.imgurEmbed){window.imgurEmbed={tasks:0};var script=document.createElement("script");script.type="text/javascript",script.async=!0,script.src="//"+source,script.charset="utf-8",document.getElementsByTagName("head")[0].appendChild(script)}window.imgurEmbed.createIframe?imgurEmbed.createIframe():imgurEmbed.tasks++;
+//# sourceMappingURL=embed.js.map
